941a2496f333ffa6c1b88176a921ab8f303f201d
Mustafa ONUŞ <69083717+onusmustafa@users.noreply.github.com>
2021-06-19 00:26:53 +0300

    Mobil

---
+++ components/footer/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="footer">
+   <footer class="bg-light text-center text-lg-start">
+  <!-- Copyright -->
+  <div class="p">
+    © 2020 Copyright / 
+    <a class="text-dark" href="#">Süleyman Demirel Üniversitesi</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background: #b9e2fc;
+   color: black;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   margin-top: 50px;
+      p{
+        text-align: center;
+      }
+ }
+</style>
---
+++ components/hero/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          Best e-commerce
+        </h1>
+        <h2 class="subtitle">
+           Made by SDU
+        </h2>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .hero {
+      display: flex;
+      align-content: center;
+      justify-content: center;
+      background: rgb(81, 186, 252);
+      margin-bottom: 50px;
+      height: 450px;
+
+    }
+    h1, h2 {
+      margin-top: 120px;
+      font-size: 30px;
+      text-align: center;
+      color: #fff;
+    }
+</style>
